3f8835dd7d5586928e8b4b4e7d77098c83375f81
Chen Quan <chenquan@osai.club>
2020-10-23 17:27:49 +0800

    Fix 测试用例bug (#4)

--- flow_test.go
+++ flow_test.go
@@ -39,7 +39,7 @@ func TestFlowBuffer(t *testing.T) {
 
 	//node2 := NewFuncNode()
 	flow := NewFlow(1)
-	flow2 := flow.FlowInWithFunc(func(in Data) (Data, bool) {
+	flow2 := flow.FlowIn(func(in Data) (Data, bool) {
 		b := in.(*bytes.Buffer)
 		data, err := ioutil.ReadAll(b)
 		var buffer bytes.Buffer
@@ -59,7 +59,7 @@ func TestFlowBuffer(t *testing.T) {
 		}
 
 	})
-	flow2.FlowInWithFunc(func(in Data) (Data, bool) {
+	flow2.FlowIn(func(in Data) (Data, bool) {
 		b := in.(*bytes.Buffer)
 
 		time.Sleep(2 * time.Millisecond)
@@ -102,11 +102,11 @@ func TestFlowBuffer(t *testing.T) {
 func TestFlowNumber(t *testing.T) {
 
 	flow := NewFlow(20)
-	flow1 := flow.FlowInWithFunc(func(in Data) (Data, bool) {
+	flow1 := flow.FlowIn(func(in Data) (Data, bool) {
 		b := in.(int)
 		return (rand.Intn(1000)) + b, true
 	})
-	flow1.FlowInWithFunc(func(in Data) (Data, bool) {
+	flow1.FlowIn(func(in Data) (Data, bool) {
 		b := in.(int)
 		return (rand.Intn(1000)) + b, true
 	})
--- go.mod
+++ go.mod
@@ -4,5 +4,4 @@ go 1.14
 
 require (
 	github.com/disintegration/imaging v1.6.2
-	golang.org/x/image v0.0.0-20200927104501-e162460cd6b5 // indirect
 )
